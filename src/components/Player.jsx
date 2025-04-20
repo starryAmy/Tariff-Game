e0831718e8ec8e5fec5a2cc1ce8afc684e434d7e
amy huang <flower850603@gmail.com>
2025-04-20 15:35:17 +0900

    now user can edit player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,32 @@
-export default function Player({ name, symbol }) {
+import { useState } from 'react'
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false)
+  const [buttonText, setButtonText] = useState('Edit')
+  const [playerName, setPlayerName] = useState(initialName)
+
+  function handleEdit() {
+    setIsEditing((editing) => !editing) // this arrow function is a callback that takes the previous state and returns the new state
+    setButtonText(isEditing ? 'Edit' : 'Save')
+  }
+
+  function handleInput (e) {
+    setPlayerName(e.target.value)
+  }
+
+  // if the player is editing, show the input field, otherwise show the player name
+  let editablePlayerName = <span className="player-name">{playerName}</span>
+  if (isEditing) {
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleInput}/>
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEdit}>{buttonText}</button>
     </li>
   )
 }
