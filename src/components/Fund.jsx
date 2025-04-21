ab1fdf7258e3e8040212271fe14b5805d2769402
amy huang <flower850603@gmail.com>
2025-04-22 04:09:10 +0900

    optimized the way to calculate remaining fund

--- src/components/Fund.jsx
+++ src/components/Fund.jsx
@@ -4,15 +4,10 @@ export default function Fund({ player, gameTurns }) {
   const [fund, setFund] = useState(20000);
 
   useEffect(() => {
-    const filteredTurns = gameTurns.filter((turn) => turn.player !== player);
-    const initialFund = 20000;
-    let damage = 0;
-    for (let i = 0; i < filteredTurns.length; i++) {
-      const { tax } = filteredTurns[i];
-      damage += 50 * tax;
-    }
-    const newFund = initialFund - damage;
-    setFund(newFund);
+    const damage = gameTurns
+      .filter((turn) => turn.player === player)
+      .reduce((sum, turn) => sum + 1000 * turn.tax, 0);
+    setFund(20000 - damage);
   }, [player, gameTurns]);
 
   return (
