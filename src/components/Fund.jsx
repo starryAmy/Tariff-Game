940972e406526ee184f8c105f64598a288042c26
amy huang <flower850603@gmail.com>
2025-04-22 17:07:13 +0900

    align text order under bar

--- src/components/Fund.jsx
+++ src/components/Fund.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Trump from "../assets/trump.png";
 import Xi from "../assets/xi.png";
+import Coin from "../assets/coin.png";
 
 export default function Fund({ player, gameTurns, deriveFund }) {
   const [fund, setFund] = useState(20000);
@@ -14,16 +15,20 @@ export default function Fund({ player, gameTurns, deriveFund }) {
   return (
     <div id="fund">
       <img src={character} alt="Trump" className="character-pic" />
-      <div className="fund-bar">
-        <div
-          className="fund-bar-fill"
-          style={{
-            width: `${Math.max((fund / 20000) * 100, 0)}%`,
-            backgroundColor: player === "Trump" ? "blue" : "red",
-          }}
-        ></div>
+      <div className="fund-bar-wrapper">
+        <div className="fund-bar-text">
+          <div className="fund-bar">
+            <div
+              className="fund-bar-fill"
+              style={{
+                width: `${Math.max((fund / 20000) * 100, 0)}%`,
+                backgroundColor: player === "Trump" ? "blue" : "red",
+              }}
+            ></div>
+          </div>
+          <p className="fund-text">{Math.max(fund, 0)} USD</p>
+        </div>
       </div>
-      <h2>{Math.max(fund, 0)} USD</h2>
     </div>
   );
 }
